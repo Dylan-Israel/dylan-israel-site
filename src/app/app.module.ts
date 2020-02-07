5ec5f85087b6cbdd3dc5bcbd48407e635e7655ce
Dylan <dylansemail310@gmail.com>
2020-02-07 17:53:49 -0500

    Added husky to run tests before commit and push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { APP_ROUTES } from './app.routes';
-import { HomeComponent } from './views/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { CourseRedirectComponent } from './views/courses/components/course-redirect/course-redirect.component';
 import { CoursesComponent } from './views/courses/courses.component';
 import { EmailSignupComponent } from './views/home/components/email-signup/email-signup.component';
-import { CourseRedirectComponent } from './views/courses/components/course-redirect/course-redirect.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { CourseRedirectComponent } from './views/courses/components/course-redir
     HomeComponent,
     CoursesComponent,
     EmailSignupComponent,
-    CourseRedirectComponent
+    CourseRedirectComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -0,0 +1,8 @@
+import { NavigationComponent } from './navigation.component';
+
+describe('Navigation Component', () => {
+  it('Exists', () => {
+    // assert
+    expect(NavigationComponent).toBeDefined();
+  });
+});
