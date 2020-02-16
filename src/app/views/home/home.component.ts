255a05598994d25c81b1acad38449737c66a3626
Dylan <dylansemail310@gmail.com>
2020-02-16 16:28:16 -0500

    Attempt 3

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-        public channelStats: any = {};
+          public channelStats: any = {};
 
   public async ngOnInit() {
     this.channelStats = await this.getChannelStats();
