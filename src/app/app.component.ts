05038323aba043afa5355f0e4f8c70cf202274fc
Dylan <dylansemail310@gmail.com>
2021-01-06 19:42:53 -0500

    removed log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   constructor() {
-    const styles = 'font-size:1.25em; padding: 5px; border-radius: 5px; font-weight: bold;'
-    console.group(`%c👨‍💻 Hi, I'm Dylan C. Israel and Thanks for Checking out my Portfolio!`, styles);
-    console.log('%c📺 YouTube - https://www.YouTube.com/CodingTutorials360', styles);
-    console.log('%c🎙️ Podcast - https://www.SelfTaughtOrNot.com', styles);
-    console.log('%c🐦Twitter - https://twitter.com/PizzaPokerGuy', styles);
-    console.log('%c📁 Github - https://github.com/pizzapokerguy', styles);
-    console.log('%c📝 LinkedIn - https://www.linkedin.com/in/dylan310/', styles);
-    console.log('%c📧 Email - DylansEmail310@gmail.com', styles);
-    console.groupEnd()
+    if (environment.production) {
+      const styles = 'font-size: 1.25em; font-weight: bold; padding: 5px;'
+      console.group(`%c👨‍💻 Hi, I'm Dylan C. Israel and Thanks for checking out my Portfolio!`, styles);
+      console.log('%c📺 YouTube - https://www.YouTube.com/CodingTutorials360', styles);
+      console.log('%c🎙️ Podcast - https://www.SelfTaughtOrNot.com', styles);
+      console.log('%c🐦Twitter - https://twitter.com/PizzaPokerGuy', styles);
+      console.log('%c📁 Github - https://github.com/pizzapokerguy', styles);
+      console.log('%c📝 LinkedIn - https://www.linkedin.com/in/dylan310/', styles);
+      console.log('%c📧 Email - DylansEmail310@gmail.com', styles);
+      console.groupEnd();
+    }
   }
 
 }
