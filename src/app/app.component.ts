0f22dd7c392aca3c76247581d9ce3e7db17ca611
Dylan <dylansemail310@gmail.com>
2021-01-06 19:10:12 -0500

    added console log portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor() {
+    const styles = 'font-size:1.25em; padding: 5px; border-radius: 5px; font-weight: bold;'
+    console.group(`%c👨‍💻 Hi, I'm Dylan C. Israel and Thanks for Checking out my Portfolio!`, styles);
+    console.log('%c📺 YouTube - https://www.YouTube.com/CodingTutorials360', styles);
+    console.log('%c🎙️ Podcast - https://www.SelfTaughtOrNot.com', styles);
+    console.log('%c🐦Twitter - https://twitter.com/PizzaPokerGuy', styles);
+    console.log('%c📁 Github - https://github.com/pizzapokerguy', styles);
+    console.log('%c📝 LinkedIn - https://www.linkedin.com/in/dylan310/', styles);
+    console.log('%c📧 Email - DylansEmail310@gmail.com', styles);
+    console.groupEnd()
+  }
+
+}
