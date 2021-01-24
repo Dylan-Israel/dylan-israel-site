c42dde1d848cbb5f83944b8e60d34a769b4744ac
Dylan <dylansemail310@gmail.com>
2021-01-23 20:45:14 -0500

    added a few tests

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -5,4 +5,19 @@ describe(FooterComponent.name, () => {
     // assert
     expect(FooterComponent).toBeDefined();
   });
+
+  describe('general', () => {
+    let component: FooterComponent;
+
+    it('sets the year to current', () => {
+      // arrange
+      spyOn(window, 'Date').and.callThrough()
+
+      // act
+      component = new FooterComponent();
+
+      // assert
+      expect(window.Date).toHaveBeenCalled();
+    });
+  });
 });
---
+++ src/app/components/github-info/github-info.component.spec.ts
@@ -0,0 +1,8 @@
+import { GithubComponent } from './github-info.component';
+
+describe(GithubComponent.name, () => {
+  it('exists', () => {
+    // assert
+    expect(GithubComponent).toBeDefined();
+  });
+});
